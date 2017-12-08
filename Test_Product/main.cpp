24eadce359826dd8d838407e4bbd1cb7714908cb
David Fergusson <davidfergusson@df-mb-rs.is.ed.ac.uk>
2017-12-08 16:07:38 +0000

    Initial Commit

---
+++ Test_Product/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Test_Product
+//
+//  Created by David Fergusson on 08/12/2017.
+//  Copyright © 2017 David Fergusson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
